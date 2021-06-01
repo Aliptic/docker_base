a653c5a6dcc7bf6786a2dc73a35fa20fd485d121
Ronan Martin <martin.ronan@gmail.com>
2021-06-01 15:57:19 +0200

    Fix path for composer create-project

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -3,7 +3,7 @@
 # Install Symfony if the folder folder not exists
 if [[ ! -d "/var/www/html/symfony/bin" ]]; then
   echo "Symfony installation"
-  composer create-project symfony/website-skeleton:"^4.4" symfony
+  composer create-project symfony/website-skeleton:"^4.4" .
   composer require form validator apache-pack profiler-pack
 else
   echo "Symfony project already exists"
